4a61e9d419aabf3c416883d90d4c43943a885a52
Hunter Wiegand <hunter_wiegand@yahoo.com>
2020-02-26 19:54:12 -0800

    Saved concurrently and edited server.js

--- scribble/server.js
+++ scribble/server.js
@@ -35,7 +35,7 @@ app.use(express.json());
 // Files found in client/build
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/public"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
   });
 };
@@ -55,7 +55,7 @@ mongoose.connect(
   }
 );
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
